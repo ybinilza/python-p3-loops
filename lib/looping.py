e4954a307d30ed08a7fc6f940f3687619aed8677
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-07-27 15:42:27 -0400

    Lab completed

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i=10;
+    while i>0:
+        
+        print(f"{i}")
+        i=i-1;
+    
+    print("Happy New Year!");
+        
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    int_list = [no * no for no in int_list]
+    return int_list;
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i=1;
+    while i<=100:
+        if i%3 ==0 and i%5 == 0 :
+            print("FizzBuzz");
+        elif i%3==0:
+            print("Fizz");
+        elif i%5 == 0:
+            print("Buzz")
+        else:
+            print(f"{i}");
+        i=i+1;
+    
